5230b2232fb3afd87806f6867276d406df367ec5
MitusovAA <alexmitusov28@gmail.com>
2023-05-25 01:59:28 +0300

    mob lab6

--- myapplication/src/main/java/com/mitusov/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/mitusov/myapplication/MainActivity.java
@@ -3,12 +3,19 @@ package com.mitusov.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.marginlayout);
+        setContentView(R.layout.activity_main);
+        TextView myTextView1 = findViewById(R.id.myText);
+        myTextView1.setText("New text in TextView");
+        Button myBtn = findViewById(R.id.myBtn);
+        myBtn.setText("My button");
+        myBtn.setEnabled(false);
     }
 }
