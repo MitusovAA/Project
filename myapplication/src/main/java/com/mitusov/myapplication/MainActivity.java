5e6616cb6b4dd1a1896e8cbe20e75cc6d6bc1c7c
MitusovAA <alexmitusov28@gmail.com>
2023-05-25 02:41:26 +0300

    mob lab7

--- myapplication/src/main/java/com/mitusov/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/mitusov/myapplication/MainActivity.java
@@ -3,19 +3,41 @@ package com.mitusov.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView myTextView1 = findViewById(R.id.myText);
-        myTextView1.setText("New text in TextView");
-        Button myBtn = findViewById(R.id.myBtn);
-        myBtn.setText("My button");
-        myBtn.setEnabled(false);
+
+        tvOut = findViewById(R.id.tvOut);
+        btnOk = findViewById(R.id.btnOk);
+        btnCancel = findViewById(R.id.btnCancel);
+
+
+        View.OnClickListener oclBtnOK = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        };
+        btnOk.setOnClickListener(oclBtnOK);
+
+        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvOut.setText("Нажата кнопка Cancel");
+
+            }
+        };
+        btnCancel.setOnClickListener(oclBtnCancel);
     }
 }
